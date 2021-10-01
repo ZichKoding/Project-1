e069d59512ab9b2c34f540961335799c7dca2d67
Brock-Merritt <88811836+Brock-Merritt@users.noreply.github.com>
2021-09-30 20:48:57 -0400

    initial sharre test

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+    var searchbar
+    var btn
+
+    // btn.css('display','none')
+    // searchbar.animate({MarginTop:'',MarginLeft:''}1000, function(wooouuuo){
+    // })
+    console.log('goose')
+});
+
+
+
+
+
+document.getElementById("btn").addEventListener("click", musicSearch)

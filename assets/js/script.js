e6ef958ecfa569b1871218cc14e8706d3585fa87
Chris Zichko <chriszichkocoding@gmail.com>
2021-10-03 00:07:33 -0400

    Bare minimum function to create the 3 main containers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
-const mainEl = $("main");
+function mainElDynamic() {
+    const mainEl = $("main");
 
-// The add class part look to the style.css to see what it contains
-let pastFuture = $("<div></div>").addClass("testingContainer");
+    // The add class part look to the style.css to see what it contains
+    let pastFuture = $("<div></div>").addClass("testingContainer");
 
-mainEl.append(pastFuture);
+    // Artworkd dynamics
+    let artEl = $("<div></div>").addClass("testingContainer");
+
+    // Music video dynamic
+    let musicEl = $("<div></div>").addClass("testingContainer");
+
+    // Appending the 3 main contents to the page.
+    mainEl.append(pastFuture);
+    mainEl.append(artEl);
+    mainEl.append(musicEl);
+}
+
+mainElDynamic();

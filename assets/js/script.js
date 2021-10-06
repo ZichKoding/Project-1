62b7c9d69c44b0aca6dbcf24e3658df89ae4171c
Chris Zichko <chriszichkocoding@gmail.com>
2021-10-05 23:14:26 -0400

    Was able to correctly pull the data endpoints for the video and embed it. Made some other changes to the css and html too

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,41 @@
-// Trying to pull the JSON formated data endpoints 
-// However it's only returning xml data.
-// Can we use xml data and if so how?
-// Might have to look for another music video api
-// YouTube Data API key = &key=AIzaSyAoAuZidiWzXoMS16M-thE7TaDJUGlS34w
+// let searchEl = $("#searchBar").text();
+// let testMBKey = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelType=any&eventType=completed&maxResults=10&order=relevance&q=' + searchEl + '&key=' + config.youtube.brock;
 
-
-
-// let musicbrainzKey = "&token=DVnA1yF1utvshelsmWupGyyTK4DOhHdCbWBEzH7P";
-// let musicBrainz =  "https://musicbrainz.org/ws/2/artist?query=Chuck Berry&limit=5&offset=3" + musicbrainzKey;
-// let testMBKey = "https://musicbrainz.org/ws/2/artist?query=name:chuckberry"+ musicbrainzKey;
-// let testMBKey = "https://itunes.apple.com/search?term=all+star";
-let searchEl = $(".search-bar").val();
-let testMBKey = "https://www.googleapis.com/youtube/v3/search?part=snippet&video=" + $(".search-bar").val() + "&key=" + config.youtube;
 
 function mainElDynamic() {
-    const mainEl = $("main");
-
+    const mainEl = $("#mainContainer");
     // The add class part look to the style.css to see what it contains
     let pastFuture = $("<div></div>").addClass("testingContainer");
-
-    // Artworkd dynamics
-    let artEl = $("<div></div>").addClass("testingContainer");
-
     // Music video dynamic
-    let musicEl = $("<div></div>").addClass("testingContainer");
-
+    let musicEl = $("<iframe></iframe>").addClass("music-video");
+    musicEl.attr("id", "searchedVid");
     // Appending the 3 main contents to the page.
     mainEl.append(pastFuture);
-    mainEl.append(artEl);
     mainEl.append(musicEl);
 };
-
-mainElDynamic();
 
 function musicBrainzData(musicbrainz) {
     fetch(musicbrainz)
     .then(response => response.json())
     .then(function(data) {
+        // logging data to find endpoints
         console.log(data);
-        console.log(data.items[0].id.videoId);
-        console.log(data.items[0].snippet.channelId);
-        console.log("https://www.youtube.com/watch?v=" + data.items[0].id.videoId + "&ab_channel=" + data.items[0].snippet.channelId);
-        let mainVidEl = "https://www.youtube.com/watch?v=" + data.items[0].id.videoId + "&ab_channel=" + data.items[0].snippet.channelId;
+        // link to the youtube page
+        console.log("https://www.youtube.com/watch?v=" + data.items[0].id.videoId + "&ab_channel=" + data.items[0].snippet.channelTitle);
+        // variable for embedding the video
+        let mainVidEl = "https://www.youtube.com/embed/" + data.items[0].id.videoId;
+        // giving iframe the src path for the video
         $("#searchedVid").attr("src", mainVidEl);
     });
 };
 
-$(".search-button").click(() =>{
+
+$("button").click((event) => {
+    event.preventDefault();
+    // added the variables here so everything will be loaded correctly. 
+    let searchEl = $("#searchBar").val();
+    let testMBKey = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelType=any&maxResults=10&order=relevance&q=' + searchEl + '&type=video&key=' + config.youtube.brock;
+    // dynamically loading the video to the html
+    mainElDynamic();
     musicBrainzData(testMBKey);
 });
-musicBrainzData(testMBKey);
--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 var config = {
     musicbrainz: "DVnA1yF1utvshelsmWupGyyTK4DOhHdCbWBEzH7P",
     theaudiodb: "523532",
-    youtube: "AIzaSyAoAuZidiWzXoMS16M-thE7TaDJUGlS34w"
+    youtube: {
+        chris: "AIzaSyAoAuZidiWzXoMS16M-thE7TaDJUGlS34w",
+        brock: "AIzaSyAN_zYs3ePlWi4ZuaYs0qy25SH-mxvyxnM",
+        kendall: "AIzaSyC-A-ettEUwHgiVTsqEHq0-7Hc85B7Bngc",
+        brynne: "AIzaSyCjBnsYFBCcujCC4QHt5DY8CK4cSFIjYYM"
+    }
 };

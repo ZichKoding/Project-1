a2a658e2f0669b2eb43771bb5e61968f17d8f73a
Chris Zichko <chriszichkocoding@gmail.com>
2021-10-06 22:47:32 -0400

    For looping through events and displaying

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-// let searchEl = $("#searchBar").text();
-// let testMBKey = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelType=any&eventType=completed&maxResults=10&order=relevance&q=' + searchEl + '&key=' + config.youtube.brock;
+const mainEl = $("#mainContainer");
+let pastFuture = $("<div></div>").addClass("testingContainer").attr("id", "eventContainer");
+let musicEl = $("<iframe></iframe>").addClass("music-video").attr("id", "searchedVid");
 
 
 // allows pressing the enter key as an option for searching in addition to the search button
@@ -10,30 +11,17 @@ $("#searchBaR").keypress(function(event) {
     }
 });
 
-
-function mainElDynamic() {
-    const mainEl = $("#mainContainer");
-    // The add class part look to the style.css to see what it contains
-    let pastFuture = $("<div></div>").addClass("testingContainer");
-    // Music video dynamic
-    let musicEl = $("<iframe></iframe>").addClass("music-video");
-    musicEl.attr("id", "searchedVid");
-    // Appending the 3 main contents to the page.
-    mainEl.append(pastFuture);
-    mainEl.append(musicEl);
-};
-
 function musicVideoData(musicVid) {
     fetch(musicVid)
     .then(response => response.json())
     .then(function(data) {
         // link to the youtube page
         console.log("https://www.youtube.com/watch?v=" + data.items[0].id.videoId + "&ab_channel=" + data.items[0].snippet.channelTitle);
-        getEvents(data.items[0].snippet.channelTitle);
         // variable for embedding the video
         let mainVidEl = "https://www.youtube.com/embed/" + data.items[0].id.videoId + "?autoplay=1";
         // giving iframe the src path for the video
-        $("#searchedVid").attr("src", mainVidEl);
+        musicEl.attr("src", mainVidEl);
+        mainEl.append(musicEl);
     });
 };
 
@@ -49,11 +37,31 @@ function getEvents(searchTerm){
             data.events[0].datetime_local,
             data.events[0].url
             );
+
+        for(let i = 0; i < data.events.length || i > 4; i++) {
+            // Looping the event dates and displaying the above the list
+            let pastFutureUL = $("<ul></ul>").attr("id", "eventDate").text("Date: " + data.events[i].datetime_local);
+            // Looping through the title, address, and url and displaying them.
+            // Title
+            let eventTitle = $("<li></li>").text(`Title: ${data.events[i].title}`);
+            pastFutureUL.append(eventTitle);
+            // Address
+            let eventAddress = $("<li></li>").text(`Address: ${data.events[0].venue.extended_address}`);
+            pastFutureUL.append(eventAddress);
+            // URL
+            let uRL = $("<a></a>").attr("src", data.events[0].url).text("https://seatgeek.com/");
+            let eventURL = $("<li></li>").text(`URL: `).append(uRL);
+            pastFutureUL.append(eventURL);
+
+            // Appending the <ul>
+            pastFuture.append($(`<h2></h2>`).text(`${searchTerm} Events`));
+            pastFuture.append(pastFutureUL);
+            mainEl.append(pastFuture);
+        }
     });
 };
 
 // itunes api to get artist name and send to getEvents
-// still looking on how to search for an artist by track name
 function getArtistName(searchTerm) {
     fetch(`https://itunes.apple.com/search?term=${searchTerm}&limit=10`)
     .then(response => response.json())
@@ -61,7 +69,7 @@ function getArtistName(searchTerm) {
         console.log(data.results[0].artistName);
         let returnedArtist = data.results[0].artistName;
         getEvents(returnedArtist);
-    })
+    });
 };
 
 
@@ -71,7 +79,7 @@ $("button").click((event) => {
     let searchEl = $("#searchBar").val();
     let testMBKey = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelType=any&maxResults=10&order=relevance&q=' + searchEl + '&type=video&key=' + config.youtube.chris;
     // dynamically loading the video to the html
-    mainElDynamic();
     musicVideoData(testMBKey);
+    // retrieving artist name from iTunes api
     getArtistName(searchEl);
 });
